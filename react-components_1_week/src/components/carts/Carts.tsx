07a622606fc53c8bbabb0a6dbfe9e8cc94665859
Asel <asel.alimbaeva.kk@gmail.com>
2022-09-28 17:01:41 +0300

    feat: add style card

--- react-components_1_week/src/components/carts/Carts.tsx
+++ react-components_1_week/src/components/carts/Carts.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react';
 import { CartsT } from 'Types';
+import './carts.css';
 
 interface CartsProps {
   carts: CartsT;
@@ -10,11 +11,16 @@ export function Carts(carts: CartsProps): ReactElement {
     <div className="cart-item">
       <div className="cart-item__image">
         <img src={carts.carts.image} alt={carts.carts.category} />
-        <img src="https://i.ibb.co/NYwBYs7/medal.png" alt="medal" />
       </div>
+      <img className="medal" src="https://i.ibb.co/NYwBYs7/medal.png" alt="medal" />
+      <img
+        className="checked"
+        src="https://i.ibb.co/HCdCQGK/gui-check-yes-icon-157194.png"
+        alt="gui-check-yes-icon-157194"
+      />
       <div className="cart-item__text">
         <h3>{carts.carts.title}</h3>
-        <p>{carts.carts.price}</p>
+        <p>{carts.carts.price} P</p>
         <div className="cart-item__rating">
           <h4>Rating</h4>
           <p>
@@ -26,9 +32,24 @@ export function Carts(carts: CartsProps): ReactElement {
         </div>
       </div>
       <div className="cart-item__pay">
-        <img src="https://i.ibb.co/StjPmmN/basket.png" alt="basket" />
-        <img src="https://i.ibb.co/c6kx2PV/heart-love-wite3.png" alt="heart-love-wite3" />
-        <img src="https://i.ibb.co/VN2M7Wn/heart.png" alt="heart" />
+        <div>
+          <img src="https://i.ibb.co/StjPmmN/basket.png" alt="basket" />
+          <img
+            className="white-hart"
+            src="https://i.ibb.co/c6kx2PV/heart-love-wite3.png"
+            alt="heart-love-wite3"
+          />
+          <img className="red-hart" src="https://i.ibb.co/VN2M7Wn/heart.png" alt="heart" />
+        </div>
+        <div className="count-block">
+          <button>
+            <img src="https://i.ibb.co/Z62ZWsG/minus.png" alt="minus" />
+          </button>
+          <span className="count">1</span>
+          <button>
+            <img src="https://i.ibb.co/rsNWgF1/Plus.png" alt="Plus" />
+          </button>
+        </div>
       </div>
     </div>
   );
