a04a0b5450f70403fe78feec69308fd2d3963429
Asel <asel.alimbaeva.kk@gmail.com>
2022-10-06 19:23:18 +0300

    feat: add test for forms file

--- react-components_1_week/src/components/formCarts/FormCarts.tsx
+++ react-components_1_week/src/components/formCarts/FormCarts.tsx
@@ -7,7 +7,7 @@ export default class FormCarts extends React.Component {
   render() {
     if (carts.length !== 0) {
       return (
-        <div className="form-carts">
+        <div data-testid="form-carts" className="form-carts">
           {carts.map((item, key) => {
             return <CreatFormCart card={item} key={key} />;
           })}
