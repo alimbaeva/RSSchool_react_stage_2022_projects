117c8a50f9a5e26b2608b042675907c1acd05f2e
Asel <asel.alimbaeva.kk@gmail.com>
2022-10-14 13:32:57 +0300

    fix: refactor card trst

--- react-components_1_week/src/components/carts/Carts.test.tsx
+++ react-components_1_week/src/components/carts/Carts.test.tsx
@@ -1,28 +1,42 @@
-// import { render, screen } from '@testing-library/react';
-// import React from 'react';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
 // import { carts } from 'data/dataCart';
-// import Carts from './Carts';
+import Carts from './Carts';
+import { Character } from 'rickiMartyTypes';
 
-// describe('when render Carts', () => {
-//   it('should contain an expacted img', () => {
-//     render(<Carts carts={carts[0]} />);
-//     const basket = document.querySelector('.white-hart') as HTMLImageElement;
-//     expect(basket.alt).toContain('heart-love-wite3');
+describe('when render Carts', () => {
+  const response: Character = {
+    created: '2017-11-04T18:48:46.250Z',
+    episode: ['51'],
+    gender: 'Male',
+    id: 1,
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    location: { name: 'Citadel of Ricks', url: 'https://rickandmortyapi.com/api/location/3' },
+    name: 'Rick Sanchez',
+    origin: { name: 'Earth (C-137)', url: 'https://rickandmortyapi.com/api/location/1' },
+    species: 'Human',
+    status: 'Alive',
+    type: '',
+    url: 'https://rickandmortyapi.com/api/character/1',
+  };
 
-//     const medal = document.querySelector('.medal') as HTMLImageElement;
-//     expect(medal.alt).toContain('medal');
-//   });
+  it('should contain an expacted img', async () => {
+    render(<Carts carts={response} />);
+    const basket = document.querySelector('.white-hart') as HTMLImageElement;
+    expect(basket.alt).toContain('heart-love-wite3');
 
-//   it('should contain an expacted img', () => {
-//     render(<Carts carts={carts[0]} />);
-//     const testImage = document.querySelector('img') as HTMLImageElement;
-//     expect(testImage.src).toContain('https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
-//   });
+    const medal = document.querySelector('.medal') as HTMLImageElement;
+    expect(medal.alt).toContain('medal');
+  });
 
-//   it('should contain an expacted Title h3', () => {
-//     render(<Carts carts={carts[0]} />);
-//     expect(
-//       screen.getByText('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
-//     ).toBeInTheDocument();
-//   });
-// });
+  it('should contain an expacted img', () => {
+    render(<Carts carts={response} />);
+    const testImage = document.querySelector('img') as HTMLImageElement;
+    expect(testImage.src).toContain('https://rickandmortyapi.com');
+  });
+
+  it('should contain an expacted Title h3', () => {
+    render(<Carts carts={response} />);
+    expect(screen.getByText(/status/i)).toBeInTheDocument();
+  });
+});
